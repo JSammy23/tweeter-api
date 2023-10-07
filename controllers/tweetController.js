b7df3b9d4425d078e99ab32bbc625d693bbc122f
Jordan <jordansampson23@gmail.com>
2023-10-07 13:40:35 -0500

    Cleanup createTweet function.

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -4,14 +4,13 @@ const asyncHandler = require("express-async-handler");
 
 /* Tweet Routes Needed */
 // CRUD operations
-// 
 
 
 exports.validateAndSanitizeTweet = [
     body('text')
         .trim() 
         .isLength({ min: 1, max: 500 }).withMessage('Tweet content must be between 1 and 500 characters.')
-        .escape(), // Escapes any HTML special characters
+        .escape(), 
     body('replyTo')
         .optional()
         .isMongoId().withMessage('Invalid ID format for reply tweet.'),
@@ -19,31 +18,19 @@ exports.validateAndSanitizeTweet = [
 
 // Create Tweet
 exports.create_tweet = asyncHandler(async (req, res, next) => {
-    // Perform validation checks
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    console.log('Logged in user:', req.user);
 
-    try {
-        // Create the tweet
-        const tweet = new Tweet({
-            author: req.user._id, 
-            text: req.body.text,
-            replyTo: req.body.replyTo ? req.body.replyTo : null,
-            // Other fields like date, likesCount, etc., will be filled with default values
-        });
+    const tweet = new Tweet({
+        author: req.user._id, 
+        text: req.body.text,
+        replyTo: req.body.replyTo ? req.body.replyTo : null,
+    });
 
-        // Save the tweet
-        const savedTweet = await tweet.save();
-
-        // Respond with the saved tweet
-        res.status(201).json(savedTweet);
-
-    } catch (error) {
-        res.status(500).json({ message: "An error occurred while creating the tweet." });
-    }
+    const savedTweet = await tweet.save();
+    res.status(201).json(savedTweet);
 });
 
 // Update Tweet
