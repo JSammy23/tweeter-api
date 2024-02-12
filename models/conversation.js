11a344f78006cbcf878f27b747ff288b87a34a5d
Jordan <jordansampson23@gmail.com>
2024-02-12 13:57:00 -0600

    Remove validator.

--- models/conversation.js
+++ models/conversation.js
@@ -6,12 +6,6 @@ const ConversationSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     }],
-    validate: {
-        validator: function (val) {
-            return val.length <= 10;
-        },
-        message: props => `${props.value.length} recipients exceeds maximum limit of 10.`
-    },
     deletedByUsers: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
